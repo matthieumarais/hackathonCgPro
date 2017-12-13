9005b7098f22f356d2eddaa22d6dbc01d9060802
Matthieu Marais <mqtthieu@gmail.com>
2017-12-13 23:26:26 +0100

    api call

---
+++ src/components/Api.vue
@@ -0,0 +1,39 @@
+/* eslint-disable */
+<template>
+<div id="Api">
+  <ul>
+    <li v-for="item in assets">{{ item.name }}</li>
+  </ul>
+</div>
+</template>
+
+/* eslint-disable */
+<script>
+import axios from "axios";
+export default {
+  name: "Api",
+  data() {
+    return {
+      assets: [],
+      errors: []
+    };
+  },
+
+  // Fetches posts when the component is created.
+  async created() {
+    try {
+      const response = await axios.get(
+        `https://ulnjbgo4dl.execute-api.eu-central-1.amazonaws.com/dev/hackaton/user/1/asset`
+      );
+      this.assets = response.data;
+    } catch (e) {
+      this.errors.push(e);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Api from '@/components/Api'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios';
+
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [{
+            path: '/',
+            component: HelloWorld
+        },
+        {
+            path: '/api',
+            component: Api
+        }
+    ]
 })
